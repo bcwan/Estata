996f77c78c48026707694ca3c0551fc318d8d0f1
bcwan <bcwan8@gmail.com>
2021-01-22 10:55:29 -0800

    Remove comment

--- src/main/java/com/bcwan/estata/EstataApplication.java
+++ src/main/java/com/bcwan/estata/EstataApplication.java
@@ -3,7 +3,6 @@ package com.bcwan.estata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Spring Boot main file
 @SpringBootApplication
 public class EstataApplication {
 
