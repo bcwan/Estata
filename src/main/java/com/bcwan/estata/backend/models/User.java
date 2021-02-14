80ae8c60e3d1980a502c28c977a405038d56524b
bcwan <bcwan8@gmail.com>
2021-02-13 23:13:35 -0800

    Add dependencies to User model

--- src/main/java/com/bcwan/estata/backend/models/User.java
+++ src/main/java/com/bcwan/estata/backend/models/User.java
@@ -1,5 +1,20 @@
 package com.bcwan.estata.backend.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users",
+       uniqueConstraints = {
+         @UniqueConstraint(columnNames = "username"),
+         @UniqueConstraint(columnNames = "email")
+       }
+      )
 public class User {
   
 }
