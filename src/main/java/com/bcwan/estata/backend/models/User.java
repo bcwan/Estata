e2be7d28f566fb611d78082b86482d708e2bb0eb
bcwan <bcwan8@gmail.com>
2021-02-13 23:23:42 -0800

    Add getter and setters for User entity model

--- src/main/java/com/bcwan/estata/backend/models/User.java
+++ src/main/java/com/bcwan/estata/backend/models/User.java
@@ -40,4 +40,53 @@ public class User {
             )
   private Set<Role> roles = new HashSet<>();
 
+  public User() {
+
+  }
+
+  public User(String username, String email, String password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Set<Role> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(Set<Role> roles) {
+    this.roles = roles;
+  }
 }
