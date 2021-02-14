dc416895b1311934119be0249ebf50abcaf590a0
bcwan <bcwan8@gmail.com>
2021-02-14 15:42:41 -0800

    Add dependencies for WebSecurityConfig

--- src/main/java/com/bcwan/estata/backend/security/WebSecurityConfig.java
+++ src/main/java/com/bcwan/estata/backend/security/WebSecurityConfig.java
@@ -1,5 +1,23 @@
 package com.bcwan.estata.backend.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.bcwan.estata.backend.security.jwt.AuthEntryPointJwt;
+import com.bcwan.estata.backend.security.jwt.AuthTokenFilter;
+import com.bcwan.estata.backend.security.jwt.UsernamePasswordAuthenticationFilter;
+
 // works with WebSecurityConfigurerAdapter in Spring Security
 public class WebSecurityConfig {
   
