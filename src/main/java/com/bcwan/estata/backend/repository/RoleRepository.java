5c1acda37fec0c89faa6fd5e1576b9266fda8a9d
bcwan <bcwan8@gmail.com>
2021-02-14 13:10:55 -0800

    Add RoleRepository interface

--- src/main/java/com/bcwan/estata/backend/repository/RoleRepository.java
+++ src/main/java/com/bcwan/estata/backend/repository/RoleRepository.java
@@ -1,5 +1,13 @@
 package com.bcwan.estata.backend.repository;
 
-public class RoleRepository {
-  
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bcwan.estata.backend.models.ERole;
+import com.bcwan.estata.backend.models.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+  Optional<Role> findByName(ERole name);
 }
