f80b30b1957fbe5f7e587625cf43c26407eb6b23
bcwan <bcwan8@gmail.com>
2021-02-13 23:20:59 -0800

    Set up attributes of User model

--- src/main/java/com/bcwan/estata/backend/models/User.java
+++ src/main/java/com/bcwan/estata/backend/models/User.java
@@ -16,5 +16,28 @@ import javax.validation.constraints.Size;
        }
       )
 public class User {
-  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Size(max = 20)
+  private String username;
+
+  @NotBlank
+  @Size(max = 50)
+  @Email
+  private String email;
+
+  @NotBlank
+  @Size(max = 120)
+  private String password;
+
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable( name = "user_roles",
+              joinColumns = @JoinColumn(name = "user_id"),
+              inverseJoinColumns = @JoinColumn(name = "role_id")
+            )
+  private Set<Role> roles = new HashSet<>();
+
 }
