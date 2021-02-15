6f444c950542317cb2bf12d501c9df3791443918
bcwan <bcwan8@gmail.com>
2021-02-14 17:30:42 -0800

    Add more details for UserDetailsImpl

--- src/main/java/com/bcwan/estata/backend/security/services/UserDetailsImpl.java
+++ src/main/java/com/bcwan/estata/backend/security/services/UserDetailsImpl.java
@@ -12,6 +12,37 @@ import org.springframework.security.core.userdetails.UserDetails;
 import com.bcwan.estata.backend.models.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-public class UserDetailsImpl {
+public class UserDetailsImpl implements UserDetails {
   
+  private static final long serialVersionUID = 1L;
+  private Long id;
+  private String username;
+  private String email;
+
+  @JsonIgnore
+  private String password;
+
+  private Collection <? extends GrantedAuthority> authorities;
+  
+  public UserDetailsImpl(Long id, String username, String email, String password,
+      Collection<? extends GrantedAuthority> authorities) {
+    this.id = id;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.authorities = authorities;
+  }
+
+  public static UserDetailsImpl build(User user) {
+    List<GrantedAuthority> authorities = user.getRoles().stream()
+        .map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList());
+
+    return new UserDetailsImpl(user.getId(), user.getUsername(), user.getEmail(), user.getPassword(), authorities);
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return authorities;
+  }
+
 }
