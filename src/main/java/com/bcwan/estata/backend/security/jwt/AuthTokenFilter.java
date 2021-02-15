65827ec5d8a2208b3bc150511e0b742570fe5e97
bcwan <bcwan8@gmail.com>
2021-02-14 18:13:58 -0800

    Add imports for AuthTokenFilter

--- src/main/java/com/bcwan/estata/backend/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/bcwan/estata/backend/security/jwt/AuthTokenFilter.java
@@ -1,5 +1,23 @@
 package com.bcwan.estata.backend.security.jwt;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
 public class AuthTokenFilter {
   
 }
