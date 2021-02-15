efc66c48991fbec031c3a45bb36384a80d1b8772
bcwan <bcwan8@gmail.com>
2021-02-14 17:56:03 -0800

    Add dependencies for UserDetailsServiceImpl

--- src/main/java/com/bcwan/estata/backend/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/bcwan/estata/backend/security/services/UserDetailsServiceImpl.java
@@ -1,5 +1,16 @@
 package com.bcwan.estata.backend.security.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bcwan.estata.backend.models.User;
+import com.bcwan.estata.backend.repository.UserRepository;
+
+@Service
 public class UserDetailsServiceImpl {
   
 }
