496697ec11490565bb1298fda4879c5dd00312ca
bcwan <bcwan8@gmail.com>
2021-02-14 17:18:40 -0800

    Add dependencies for UserDetailsImpl file

--- src/main/java/com/bcwan/estata/backend/security/services/UserDetailsImpl.java
+++ src/main/java/com/bcwan/estata/backend/security/services/UserDetailsImpl.java
@@ -1,5 +1,17 @@
 package com.bcwan.estata.backend.security.services;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.bcwan.estata.backend.models.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class UserDetailsImpl {
   
 }
