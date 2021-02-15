188f686604ead0799d0dc4dfb8058aa5f71f9a4b
bcwan <bcwan8@gmail.com>
2021-02-14 16:33:36 -0800

    Add WebSecurityConfig methods

--- src/main/java/com/bcwan/estata/backend/security/WebSecurityConfig.java
+++ src/main/java/com/bcwan/estata/backend/security/WebSecurityConfig.java
@@ -16,9 +16,48 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.bcwan.estata.backend.security.jwt.AuthEntryPointJwt;
 import com.bcwan.estata.backend.security.jwt.AuthTokenFilter;
-import com.bcwan.estata.backend.security.jwt.UsernamePasswordAuthenticationFilter;
+import com.bcwan.estata.backend.security.services.UserDetailsServiceImpl;
 
 // works with WebSecurityConfigurerAdapter in Spring Security
-public class WebSecurityConfig {
-  
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+    prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+  @Autowired
+  UserDetailsServiceImpl userDetailsService;
+
+  @Autowired
+  private AuthEntryPointJwt unauthorizedHandler;
+
+  @Bean
+  public AuthTokenFilter authenticationJwtTokenFilter() {
+    return new AuthTokenFilter();
+  }
+
+  @Override
+  public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+    authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+  }
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+        .antMatchers("/api/auth/**").permitAll().antMatchers("/api/test/**").permitAll().anyRequest().authenticated();
+
+    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+  }
 }
