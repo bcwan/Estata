3c40f07f431bb8e272b41abab72b118c8b18fc75
bcwan <bcwan8@gmail.com>
2021-02-27 13:32:55 -0800

    Retrieve data from the server

---
+++ src/frontend/services/auth/auth_header.js
@@ -0,0 +1,10 @@
+// Retrieve data from server
+export default function authHeader() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (user && user.accessToken) {
+    return { Authorization: 'Bearer ' + user.accessToken };
+  } else {
+    return {};
+  }
+}
