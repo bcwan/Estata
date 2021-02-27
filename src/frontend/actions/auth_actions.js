fecf2eac6200ca06bf752cd888a4e0b439a66794
bcwan <bcwan8@gmail.com>
2021-02-27 15:35:32 -0800

    Add register API call

--- src/frontend/actions/auth_actions.js
+++ src/frontend/actions/auth_actions.js
@@ -35,6 +35,17 @@ export const logoutSuccess = () => ({
 
 export const register = (username, email, password) => (dispatch) => {
   return AuthService.register(username, email, password)
-
-}
+    .then((response) => {
+            dispatch(registerSuccess());
+            dispatch(setMessage(response.data.message));
+            return Promise.resolve();
+          }, 
+          (error) => {
+            const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+            dispatch(registerFail());
+            dispatch(setMessage(message));
+            return Promise.reject();
+          }
+    );
+};
 
