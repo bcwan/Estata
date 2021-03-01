be48a67a42785b02d179d52f1bd6a11ba276c889
bcwan <bcwan8@gmail.com>
2021-02-28 17:50:39 -0800

    Update LoginFormContainer

--- src/frontend/components/session/login_form_container.js
+++ src/frontend/components/session/login_form_container.js
@@ -2,3 +2,13 @@ import { connect } from "react-redux";
 import LoginForm from './login_form';
 import { login } from '../../actions/auth_actions';
 
+const mSTP = (state) => ({
+  isLoggedIn: state.auth.isLoggedIn,
+  message: state.messages.message
+});
+
+const mDTP = (dispatch) => ({
+
+});
+
+export default connect(mSTP, mDTP)(LoginForm);
