ead58dc93aa00edd2fe3d7ca4bec4fe7e170b103
bcwan <bcwan8@gmail.com>
2021-02-28 18:24:34 -0800

    Create helper functions for handling login

--- src/frontend/components/session/login_form.jsx
+++ src/frontend/components/session/login_form.jsx
@@ -1,6 +1,73 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
-export class LoginForm extends Component {
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+import CheckButton from 'react-validation/build/button';
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">This field is required!</div>
+    );
+  }
+};
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+
+    this.state = {
+      username: "",
+      password: "",
+      loading: false,
+    }
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  handleLogin(e) {
+    e.preventDefault();
+    this.setState({
+      loading: true,
+    });
+
+    this.form.validateAll();
+
+    const { history, login } = this.props;
+
+    if (this.checkBtn.context._errors.length === 0) {
+      login(this.state.username, this.state.password)
+        .then(() => {
+          history.push("/profile");
+          window.location.reload();
+        })
+        .catch(() => {
+          this.setState({
+            loading: false
+          });
+        });
+    } else {
+      this.setState({
+        loading: false,
+      });
+    }
+
+  }
+
   render() {
     return (
       <div>
--- src/frontend/components/session/login_form_container.js
+++ src/frontend/components/session/login_form_container.js
@@ -4,11 +4,11 @@ import { login } from '../../actions/auth_actions';
 
 const mSTP = (state) => ({
   isLoggedIn: state.auth.isLoggedIn,
-  message: state.messages.message
+  message: state.messages.message,
 });
 
 const mDTP = (dispatch) => ({
-
+  login: (username, password) => dispatch(login(username, password))
 });
 
 export default connect(mSTP, mDTP)(LoginForm);
