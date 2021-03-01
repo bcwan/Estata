be75ef2f1d189f031445c2a5c43cc7a5855994b5
bcwan <bcwan8@gmail.com>
2021-02-28 22:11:46 -0800

    Add event handlers for RegisterForm

--- src/frontend/components/session/register_form.jsx
+++ src/frontend/components/session/register_form.jsx
@@ -45,6 +45,39 @@ const vpassword = (value) => {
 };
 
 class RegisterForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleRegister = this.handleRegister.bind(this);
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      successful: false,
+    };
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
   render() {
     return (
       <div>
