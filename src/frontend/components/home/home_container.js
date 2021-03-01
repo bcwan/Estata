262bd536b00e18c2d98beab73f90e43f26a622be
bcwan <bcwan8@gmail.com>
2021-03-01 00:13:38 -0800

    Add HomeContainer

---
+++ src/frontend/components/home/home_container.js
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import Home from './home';
+
+const mSTP = (state) => ({
+
+});
+
+const mDTP = (dispatch) => ({
+
+});
+
+export default connect(mSTP, mDTP)(Home);
