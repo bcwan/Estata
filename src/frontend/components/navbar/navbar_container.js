c9a1a5764b49216770754ef5e6dc8ab7e493c8fb
bcwan <bcwan8@gmail.com>
2021-03-02 00:52:40 -0800

    Add logout to pass down to NavBar component

--- src/frontend/components/navbar/navbar_container.js
+++ src/frontend/components/navbar/navbar_container.js
@@ -1,12 +1,13 @@
 import { connect } from "react-redux";
 import NavBar from './navbar';
+import { logout } from '../../actions/auth_actions'
 
 const mSTP = (state) => ({
 
 });
 
 const mDTP = (dispatch) => ({
-
+  logout: () => dispatch(logout())
 });
 
 export default connect(mSTP, mDTP)(NavBar);
