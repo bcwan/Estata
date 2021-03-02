6022d75f1b96334ca789e0cb9449ec77090e42d7
bcwan <bcwan8@gmail.com>
2021-03-01 23:11:09 -0800

    Create NavBar state

--- src/frontend/components/navbar/navbar.jsx
+++ src/frontend/components/navbar/navbar.jsx
@@ -1,6 +1,22 @@
 import React, { Component } from 'react'
 
 export class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    };
+
+    this.logOut = this.logOut.bind(this);
+
+  }
+
+  logOut() {
+
+  }
+
   render() {
     return (
       <div>
