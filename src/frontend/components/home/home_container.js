a18edf619075409cd6acb6ed60c0a98a7fab8d23
bcwan <bcwan8@gmail.com>
2021-03-02 00:59:05 -0800

    Remove un-needed util function

--- src/frontend/components/home/home_container.js
+++ src/frontend/components/home/home_container.js
@@ -1,13 +1,12 @@
 import { connect } from "react-redux";
 import Home from './home';
-import { clearMessage } from '../../actions/message_actions'
 
 const mSTP = (state) => ({
 
 });
 
 const mDTP = (dispatch) => ({
-  clearMessage: () => dispatch(clearMessage()),
+
 });
 
 export default connect(mSTP, mDTP)(Home);
