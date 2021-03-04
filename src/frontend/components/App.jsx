1b84c9c68f8ec9475d26f2100a40c89d5de822fa
bcwan <bcwan8@gmail.com>
2021-03-03 21:51:08 -0800

    Add BoardMod and BoardUser components

--- src/frontend/components/App.jsx
+++ src/frontend/components/App.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
+import { Router, Switch, Route, Link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-class App extends Component {
-  render() {
+// import { history } from '../history/history';
+import NavBarContainer from './navbar/navbar';
 
+class App extends Component {
+
+  render() {
+    <div>
+      {/* <Router history={history}> */}
+      <Router>
+        <NavBarContainer />
+        <Switch>
+          
+        </Switch>
+      </Router>
+    </div>
   }
 }
 
