15eeb6ee50b16d19113b997cf77008ea8e7715f1
bcwan <bcwan8@gmail.com>
2021-03-03 23:03:16 -0800

    Add containers to Board components

---
+++ src/frontend/components/board/board_mod_container.js
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import BoardMod from './board_mod';
+
+const mSTP = (state) => ({
+
+});
+
+const mDTP = (dispatch) => ({
+
+});
+
+export default connect(mSTP, mDTP)(BoardMod);
---
+++ src/frontend/components/board/board_user_container.js
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import BoardUser from './board_user';
+
+const mSTP = (state) => ({
+
+});
+
+const mDTP = (dispatch) => ({
+
+});
+
+export default connect(mSTP, mDTP)(BoardUser);
