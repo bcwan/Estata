5e2384abdcfcad0fa34c4a07060a89015c0cc9c1
bcwan <bcwan8@gmail.com>
2021-03-03 23:12:15 -0800

    Add routes for each React component

--- src/frontend/components/App.jsx
+++ src/frontend/components/App.jsx
@@ -4,6 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 // import { history } from '../history/history';
 import NavBarContainer from './navbar/navbar';
+import BoardAdminContainer from './board/board_admin_container';
+import BoardModContainer from './board/board_mod_container';
+import BoardUserContainer from './board/board_user_container';
+import HomeContainer from './home/home_container';
+import LoginFormContainer from './session/login_form_container';
+import ProfileContainer from './session/profile_container';
+import RegisterFormContainer from './session/register_form_container';
 
 class App extends Component {
 
@@ -13,7 +20,13 @@ class App extends Component {
       <Router>
         <NavBarContainer />
         <Switch>
-          
+          <Route exact path={["/", "/home"]} component={HomeContainer} />
+          <Route exact path="/login" component={LoginFormContainer} />
+          <Route exact path="/register" component={RegisterFormContainer} />
+          <Route exact path="/profile" component={ProfileContainer} />
+          <Route path="/user" component={BoardUserContainer} />
+          <Route path="/mod" component={BoardModContainer} />
+          <Route path="/admin" component={BoardAdminContainer} />
         </Switch>
       </Router>
     </div>
