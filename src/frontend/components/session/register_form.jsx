0497313aceafcb6aedd65cd62fbb239aaba19850
bcwan <bcwan8@gmail.com>
2021-03-03 23:48:20 -0800

    Add css to forms

--- src/frontend/components/session/register_form.jsx
+++ src/frontend/components/session/register_form.jsx
@@ -3,6 +3,8 @@ import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
 import { isEmail } from "validator";
+import './form.css';
+import '../App.css';
 
 const required = (value) => {
   if (!value) {
