110da3322ad540517e2f683c9a299d7f815b0721
bcwan <bcwan8@gmail.com>
2021-03-04 21:37:38 -0800

    Move files to allow Babel and webpack to run

---
+++ src/frontend/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx', '.css' ,'*']
+  },
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
