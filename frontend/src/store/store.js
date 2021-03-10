f1ffeee50902e4adbab0277efff544e85d8fc6e7
bcwan <bcwan8@gmail.com>
2021-03-09 16:52:47 -0800

    Write comments for future use

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,6 +3,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from '../reducers/root_reducer';
 
+// invoke store
 const configureStore = (preloadedState = {}) => (
   createStore(rootReducer, preloadedState, applyMiddleware(thunk))
 );
