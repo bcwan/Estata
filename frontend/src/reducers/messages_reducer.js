894338615be55fb751b73be0895b4ab146c03196
bcwan <bcwan8@gmail.com>
2021-04-26 14:38:42 -0700

    Add reducers for redux state

---
+++ frontend/src/reducers/messages_reducer.js
@@ -0,0 +1,18 @@
+import { SET_MESSAGE, CLEAR_MESSAGE } from '../actions/message_actions';
+
+const messageReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  let nextState = Object.assign({}, oldState);
+  switch (action.type) {
+    case SET_MESSAGE:
+      nextState["message"] = action.payload;
+      return nextState;
+    case CLEAR_MESSAGE:
+      delete nextState["message"];
+      return nextState;
+    default:
+      return oldState;
+  }
+}
+
+export default messageReducer;
