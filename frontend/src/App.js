df664afc628ad00502df86cb2acf6db944c9e8d4
bcwan <bcwan8@gmail.com>
2021-04-26 14:14:55 -0700

    Add app.js with routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Router, Switch, Route, Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// import { history } from '../history/history';
+import NavBarContainer from './navbar/navbar';
+import BoardAdminContainer from './board/board_admin_container';
+import BoardModContainer from './board/board_mod_container';
+import BoardUserContainer from './board/board_user_container';
+import HomeContainer from './home/home_container';
+import LoginFormContainer from './session/login_form_container';
+import ProfileContainer from './session/profile_container';
+import RegisterFormContainer from './session/register_form_container';
+
+class App extends Component {
+
+  render() {
+    <div className="container mt-3">
+      {/* <Router history={history}> */}
+      <Router>
+        <NavBarContainer />
+        <Switch>
+          <Route exact path={["/", "/home"]} component={HomeContainer} />
+          <Route exact path="/login" component={LoginFormContainer} />
+          <Route exact path="/register" component={RegisterFormContainer} />
+          <Route exact path="/profile" component={ProfileContainer} />
+          <Route path="/user" component={BoardUserContainer} />
+          <Route path="/mod" component={BoardModContainer} />
+          <Route path="/admin" component={BoardAdminContainer} />
+        </Switch>
+      </Router>
     </div>
-  );
+  }
 }
 
 export default App;
